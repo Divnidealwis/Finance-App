9f837c65da95a428cd1194c72f599d972287023a
Divni de Alwis <divnidealwis@gmail.com>
2025-04-04 00:50:48 +0530

    feat: Implement dashboard layout with grid structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,8 +5,8 @@ import { useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar/index";
-// import Dashboard from "@/scenes/dashboard/index";
-// import Predictions from "@/scenes/predictions";
+import Dashboard from "@/scenes/dashboard/index";
+// import Predictions from "@/scenes/predictions/index";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -18,8 +18,8 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/dashboard" element={<div>Dashboard Page</div>} />
-              <Route path="/predictions" element={<div>Prediction Page</div>} />
+            <Route path="/" element={<Dashboard />} />
+            {/* <Route path="/predictions" element={<Predictions />} /> */}
             </Routes>
           </Box>
         </ThemeProvider>
